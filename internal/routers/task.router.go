9158790fd560b1a5b54c43177541881d7f86a1bd
funukonta <evanroy36@gmail.com>
2024-03-19 14:02:32 +0700

    fix : add route tasks

--- internal/routers/task.router.go
+++ internal/routers/task.router.go
@@ -13,8 +13,12 @@ func TaskRouter(g *gin.Engine, db *sqlx.DB) {
 	service := services.New_ServiceTask(repo)
 	handler := handlers.New_HandlerTask(service)
 
-	taskRoute := g.Group("/task")
+	taskRoute := g.Group("/tasks")
 	{
 		taskRoute.POST("", handler.RegisterTasks)
+		taskRoute.GET("", handler.GetTasks)
+		taskRoute.GET("/:id", handler.GetTasksById)
+		taskRoute.PUT("/:id", handler.EditTasks)
+		taskRoute.DELETE("/:id", handler.DeleteTasks)
 	}
 }
