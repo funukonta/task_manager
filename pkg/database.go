e1763fdbc30e4d4bd5e75662e9f27d51ac4ff1e7
funukonta <evanroy36@gmail.com>
2024-03-19 07:21:24 +0700

    feat : add migration

--- pkg/database.go
+++ pkg/database.go
@@ -3,16 +3,21 @@ package pkg
 import (
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 func ConnectPostgres() (*sqlx.DB, error) {
-	host := "task-manager"
-	user := "postgres"
-	password := "task-manager"
-	dbname := "task-manager"
-	ssl := "disable"
+	host := os.Getenv("DB_HOST")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASS")
+	dbname := os.Getenv("DB_NAME")
+	ssl := os.Getenv("DB_SSL")
 
 	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=%s", user, password, host, dbname, ssl)
 
@@ -26,5 +31,18 @@ func ConnectPostgres() (*sqlx.DB, error) {
 		log.Panicln("error ping", err.Error())
 	}
 
-	return db, err
+	m, err := migrate.New("file://../migrate", connStr)
+	if err != nil {
+		log.Panicln("error migrate", err.Error())
+	}
+	// err = m.Force(2)
+	// if err != nil {
+	// 	log.Panicln("error migrate force", err.Error())
+	// }
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Panicln("error migrate up", err.Error())
+	}
+
+	return db, nil
 }
